a8f478e5471b31cbae4c9c60693b0469c6c06160
vladimir.bk <befoulers@gmail.com>
2020-11-09 12:54:01 +0100

    Move icon.js to app/scripts

---
+++ app/components/scripts/icons.js
@@ -0,0 +1,7 @@
+import iconFire from '../../../assets/firewood.svg';
+
+const icons = {
+    fire: iconFire
+}
+
+export default icons;
--- scripts/icons.js
+++
@@ -1,7 +0,0 @@
-import iconBull from '../assets/bull.svg';
-
-const icons = {
-    bull: iconBull
-}
-
-export default icons;
