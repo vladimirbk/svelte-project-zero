5b11c56a444a851915b0245008581985b378dffc
vladimir.bk <befoulers@gmail.com>
2020-11-09 12:52:49 +0100

    Rollup svg plugin set to accept base64

--- rollup.config.js
+++ rollup.config.js
@@ -69,7 +69,9 @@ export default {
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
 		production && terser(),
-		svg()
+		svg({
+			base64:true
+		})
 	],
 	watch: {
 		clearScreen: false
