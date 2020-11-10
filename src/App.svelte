9bdc043fddb9babba40989f060b4d967e827ae74
vladimir.bk <befoulers@gmail.com>
2020-11-10 13:51:12 +0100

    Move global styles to App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -11,4 +11,22 @@
 </main>
 
 <style>
+	:global(html){
+		height: 100vh;
+		width: 100vw;
+	}
+	:global(body){
+		margin: 0;
+		padding: 0;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: flex-end;
+		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+	}
+	:global(.content-center){
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 </style>
